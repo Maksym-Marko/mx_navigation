4a4c18caa112682951efc48de243806f05bd0fed
Maksym Marko <maximus21@mail.ru>
2017-04-27 10:07:06 +0300

    Step back and forth

--- functions.php
+++ functions.php
@@ -16,40 +16,59 @@ function custom_post(){
 
 //navigation
 function mx_navigation( $custom_post ){
-
 	//get count posts оn the page
 	$count_posts_in_page = get_option( 'posts_per_page' );
-
 	//get this page
 	$this_page = get_query_var( 'paged' );
 	$this_page = (int) $this_page;
 	if( $this_page === 0 ) $this_page = 1;
-
 	//get count publish posts
 	$count_posts = wp_count_posts( $type = $custom_post, $perm = '' )->publish;
-
 	//set count page
 	$count_page = $count_posts / $count_posts_in_page;
 	$count_page = ceil( $count_page );
-
 	//get url
 	$host = $_SERVER['HTTP_HOST'];
 	$path = $_SERVER['REQUEST_URI'];
 	$url = 'http://' . $host . $path;
 	$url = strstr($url, 'page', true);
-
 	//loop links
 	if( $count_posts > $count_posts_in_page ){ ?>
-		<div class="mx-page_nav">
-			<?php for ( $i = 1; $i <= $count_page; $i++ ) { 
-			if( $i === $this_page ){ ?>
-				<span class="mx-active_page"><?php echo $i; ?></span>
-			<?php }
-			else{ ?>
-				<a href="<?php echo $url . 'page/' . $i . '/'; ?>"  class="mx-link_page"><?php echo $i; ?></a>
-			<?php }
-			} ?>
-		</div>
-	<?php }
-	
+
+		<!-- pagination -->
+		<nav class="mx-pagination">
+			<ul class="pagination">
+
+				<?php if( $this_page > 1 ): 
+					$prev_page = $this_page - 1;
+				?>
+
+					<li><a href="<?php echo $url . 'page/' . $prev_page . '/'; ?>" aria-label="Previous"><span aria-hidden="true" id="mx-Previous">«</span></a></li>				
+				<?php endif; ?>
+
+				<?php for ( $i = 1; $i <= $count_page; $i++ ) { 
+				if( $i === $this_page ){ ?>
+					<li class="active">
+						<a href="#" onclick="return false;"><?php echo $i; ?></a>
+					</li>
+				<?php }
+				else{ ?>					
+					<li>
+						<a href="<?php echo $url . 'page/' . $i . '/'; ?>"><?php echo $i; ?></a>
+					</li>
+
+				<?php }
+				} ?>
+
+				<?php if( $this_page < $count_page ): 
+					$next_page = $this_page + 1;
+				?>
+
+					<li><a href="<?php echo $url . 'page/' . $next_page . '/'; ?>" aria-label="Next"><span aria-hidden="true" id="mx-Next">»</span></a></li>
+				<?php endif;?>
+			</ul>
+		</nav>
+		<!-- pagination -->
+
+	<?php }	
 }
